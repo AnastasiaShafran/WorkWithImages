d2ad789056b94870ab46eef5ab121a5f9573760f
Victor Shafran <victor.shafran@equalum.io>
2016-04-25 20:23:17 +0300

    comit after image sent to FB but before list of images is shown

--- app/src/main/java/famaly/shafran/anastastia/workwithimages/MainActivity.java
+++ app/src/main/java/famaly/shafran/anastastia/workwithimages/MainActivity.java
@@ -1,42 +1,86 @@
 package famaly.shafran.anastastia.workwithimages;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.firebase.client.Firebase;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private static int RESULT_LOAD_IMG = 1;
+    private static int REQUEST_CODE_ASK_PERMISSIONS = 2;
     String imgDecodableString;
+    private Firebase myFirebaseImg;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //if (Environment.getExternalStorageState() != Environment.MEDIA_MOUNTED)
+        Firebase.setAndroidContext(this);
+        final Firebase ref = new Firebase("https://myprojectshafran.firebaseio.com/");
+        myFirebaseImg= ref.child("images");
+
+
+
     }
 
     public void loadImagefromGallery(View view) {
         // Create intent to Open Image applications like Gallery, Google Photos
+        int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE_ASK_PERMISSIONS);
+            return;
+        }
+
+
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         // Start the Intent
         startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
     }
 
+    public void saveImagetoFb(View view){
+        ImageView imgView = (ImageView) findViewById(R.id.imgView);
+        // Set the Image in ImageView after decoding the String
+
+        Bitmap bmp = ((BitmapDrawable)imgView.getDrawable()).getBitmap();
+
+        ByteArrayOutputStream bYtE = new ByteArrayOutputStream();
+
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, bYtE);
+//        bmp.recycle();
+
+        byte[] byteArray = bYtE.toByteArray();
+        String imageFile = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        myFirebaseImg.push().setValue(imageFile);
+
+
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         try {
             // When an Image is picked
             if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
@@ -56,12 +100,11 @@ public class MainActivity extends AppCompatActivity {
                 imgDecodableString = cursor.getString(columnIndex);
                 cursor.close();
 
-                File f  = new File(imgDecodableString);
-
                 ImageView imgView = (ImageView) findViewById(R.id.imgView);
                 // Set the Image in ImageView after decoding the String
+
                 imgView.setImageBitmap(BitmapFactory
-                        .decodeFile(f.getAbsolutePath()));
+                        .decodeFile(imgDecodableString));
 
             } else {
                 Toast.makeText(this, "You haven't picked Image",
