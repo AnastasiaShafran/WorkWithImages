9f3f0c3d3f055affa261b7d49889d496cf3ecc60
Victor Shafran <victor.shafran@equalum.io>
2016-04-25 19:12:28 +0300

    Initial commit

---
+++ app/src/androidTest/java/famaly/shafran/anastastia/workwithimages/ApplicationTest.java
@@ -0,0 +1,13 @@
+package famaly.shafran.anastastia.workwithimages;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
